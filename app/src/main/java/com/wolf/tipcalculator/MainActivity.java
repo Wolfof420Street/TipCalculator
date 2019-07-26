6059a0bd899abf42e2b18ca3ce07b027ae30802c
Wolfof420Street <billodida420@gmail.com>
2019-07-26 19:07:19 +0300

    adding basic functionality

--- app/src/main/java/com/wolf/tipcalculator/MainActivity.java
+++ app/src/main/java/com/wolf/tipcalculator/MainActivity.java
@@ -4,13 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText enteredAmount;
+    private SeekBar seekBar;
+    private Button calculateButton;
+    private TextView resultTextView;
+    private TextView percentageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enteredAmount = (EditText) findViewById(R.id.billAmountId);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        resultTextView = (TextView) findViewById(R.id.resultId);
+        percentageTextView = (TextView) findViewById(R.id.percentageTextView);
+
+        calculateButton.setOnClickListener(this);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                percentageTextView.setText(String.valueOf(seekBar.getProgress()) +"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
